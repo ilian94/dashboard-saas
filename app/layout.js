65caca87e06cdd027a6f0e4061885fefd61eab7a
Ilian wee <ilianwee@Mac-mini-de-Ilian.local>
2026-05-21 20:43:39 +0200

    Add Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export const metadata = {
   title: "VoiceBot AI",
@@ -9,6 +10,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-J58PNLLBZ3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-J58PNLLBZ3');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
